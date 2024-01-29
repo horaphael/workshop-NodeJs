47e1c251206c7b506912163d99b878e8ab7f09bf
rapael hoarau <raphael.hoarau@epitech.eu>
2024-01-29 17:14:33 +0400

    step 1-4

--- serveur.js
+++ serveur.js
@@ -1,24 +1,30 @@
 const http = require("http");
 const url = require('url');
-const host = 'localhost';
 
 http.createServer(function (req, res) {
-    res.write(`<html>
+    if(req.url == "/acceuil") {
+        res.write(`<html>
         <meta charset="utf-8"></meta>
         <body>
         <h1>Bienvenue sur l'accueil</h1>
-        <h1>logo:</h1>
-        <img src = "https://newsroom.ionis-group.com/wp-content/uploads/2023/09/EPI-LOGO-2023-QUADRI.png" height="142" width="450">
-        <h1>Bienvenue sur votre profile</h1>
         </body>
-        </html>`);
-    if(req.url == "/acceuil") {
+        </html>`)
         console.log("Vous êtes a l'accueil");
     }
     if(req.url == "/logo") {
+        res.write(`<html>
+        <meta charset="utf-8"></meta>
+        <h1>logo:</h1>
+        <img src = "https://newsroom.ionis-group.com/wp-content/uploads/2023/09/EPI-LOGO-2023-QUADRI.png" height="142" width="450">
+        </body>
+        </html>`)
         console.log("Vous êtes au logo");
     }
     if(req.url == "/profile") {
+        res.write(`<html>
+        <h1>Bienvenue sur votre profile</h1>
+        </body>
+        </html>`)
         console.log("Vous êtes sur votre profile");
     }
     res.end();
